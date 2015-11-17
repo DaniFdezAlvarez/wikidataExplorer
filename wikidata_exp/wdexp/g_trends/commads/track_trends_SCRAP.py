38b139fcca31bbe30324adb6eb9a40f2791ec86d
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2015-11-17 20:57:46 +0100

    Refactor scraper

--- wikidata_exp/wdexp/g_trends/commads/track_trends_SCRAP.py
+++ wikidata_exp/wdexp/g_trends/commads/track_trends_SCRAP.py
@@ -2,15 +2,12 @@
 __author__ = 'Dani'
 
 PROP_TRENDS = "trends"
-DOWNLOAD_PATH = "C:\\Users\\Dani\\repos_git\\wikidata_exp\\wikidata_exp\\files\\downloads\\g_trends"
+
+
 
 import json
 import traceback
-import time
-from os import walk
-from os import remove
-from os.path import isfile
-from os.path import join
+
 from wdexp.wikidata.commands.aliases_properties_API import PROP_LABEL, PROP_ALIASES
 from wdexp.g_trends.g_trends_scraper import GTrendsScrapper
 
@@ -21,7 +18,7 @@ class TrackTrendsCommand(object):
 
         self._in_file = source_file
         self._out_file = out_file
-        self._trends_cache = {}
+        self._scraper = GTrendsScrapper()
 
 
     def exec_command(self, string_return=False):
@@ -55,62 +52,12 @@ class TrackTrendsCommand(object):
             print "An alias done...", an_alias
         return a_dict  # In fact, not necessary. im modifying the received one
 
-        #descargar
-        #leer e introducir, en bucle
-        #borrar
 
     def _get_trends_of_label(self, a_label):
-        if a_label in self._trends_cache:
-            return self._trends_cache[a_label]
-        self._download_trend_file(a_label)
-        target_path, trend_raw_content = self._read_trend_file()
-        trend_list = self._parse_trend_content(trend_raw_content)
-        self._delete_trend_file(target_path)
-        time.sleep(2.5)
-        return trend_list
-
-    @staticmethod
-    def _download_trend_file(term):
-        GTrendsScrapper.download_page_or_term(term)
-
-    @staticmethod
-    def _read_trend_file():
-        a_path = None
-        start_time = time.time()
-        while a_path is None:
-            current_state = time.time()
-            if current_state - start_time > 30:
-                raise ValueError("Are we banned? Stop")
-            for base_path, ignore_this, the_files_array in walk(DOWNLOAD_PATH):
-                if len(the_files_array) > 0:
-                    candidate_path = join(base_path, the_files_array[0])
-                    if isfile(candidate_path) and candidate_path.endswith(".csv"):
-                        a_path = join(base_path, the_files_array[0])
-        time.sleep(1)
-        in_stream = open(a_path, "r")
-        result = in_stream.read()
-        in_stream.close()
-        return a_path, result
+        return self._scraper.get_trends_of_label(a_label)
 
 
-    @staticmethod
-    def _parse_trend_content(raw_content):
-        result = []
-        lines_array = raw_content.split("\n")
-        tracking_mode = False
-        for a_line in lines_array:
-            if tracking_mode:
-                if a_line not in ["", "\r"]:
-                    result.append((a_line.split(",")[0]).strip())
-                else:
-                    break
-            elif a_line.startswith("Búsquedas principales de"):
-                tracking_mode = True
-        return result
 
-    @staticmethod
-    def _delete_trend_file(target_path):
-        remove(target_path)
 
 
 
