22fcccdab40198073bc25d773e7d89134433978c
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2016-01-13 18:39:21 +0100

    Close #25

---
+++ wikidata_exp/wdexp/model/wikidata.py
@@ -0,0 +1,155 @@
+__author__ = 'Dani'
+
+
+
+class WikidataEntity(object):
+
+    def __init__(self, entity_id, label=None, description=None, aliases=None, properties_id=None):
+        self._id = entity_id
+        self._label = label
+        self._description = description
+        if aliases is None:
+            aliases = []
+        self._aliases = aliases
+        if properties_id is None:
+            properties_id = []
+        self._properties = properties_id
+
+
+    def __str__(self):
+        return self._id
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def aliases(self):
+        for an_alias in self._aliases:
+            yield an_alias
+
+    @property
+    def n_aliases(self):
+        return len(self._aliases)
+
+    @property
+    def properties_id(self):
+        for a_prop in self._properties:
+            yield a_prop
+
+    @property
+    def n_properties(self):
+        return len(self._properties)
+
+
+
+
+class WikidataProperty(object):
+
+    def __init__(self, property_id, label=None, description=None, aliases=None, trends=None):
+        self._id = property_id
+        self._label = label
+        self._description = description
+        if aliases is None:
+            aliases = []
+        self._aliases = aliases
+        if trends is None:
+            trends = []
+        self._trends = trends
+
+
+    def __str__(self):
+        return self._id
+
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def aliases(self):
+        for an_alias in self._aliases:
+            yield an_alias
+
+    @property
+    def n_aliases(self):
+        return len(self._aliases)
+
+
+    @property
+    def trends(self):
+        for a_trend in self._trends:
+            yield a_trend
+
+    @property
+    def n_trends(self):
+        return len(self._trends)
+
+
+##### LITERAL_TYPES
+
+TYPE_UNKOWN = -1
+TYPE_NUMBER = 0
+TYPE_STRING = 1
+
+
+class WikidataLiteral(object):
+
+    def __init__(self, value, literal_type=None):
+        self._value = value
+        self._type = literal_type
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def type(self):
+        return self._type
+
+    def __str__(self):
+        return str(self._value)
+
+
+
+class WikidataTriple(object):
+
+    def __init__(self, subject, predicate, target_object):
+        self._subject = subject
+        self._predicate = predicate
+        self._object = target_object
+
+
+    @property
+    def subject(self):
+        return self._subject
+
+
+    @property
+    def predicate(self):
+        return self._predicate
+
+    @property
+    def object(self):
+        return self._object
+
+    def __str__(self):
+        return str(self.subject) + ", " + str(self._predicate) + ", " + str(self.object)
+
+
